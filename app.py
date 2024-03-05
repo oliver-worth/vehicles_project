2655391de48cfed6cd7321f4c117bc2b569fbd77
Oliver Worth <oworth17@gmail.com>
2024-03-04 23:04:14 -0500

    updated files

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly_express as px
 import plotly.graph_objects as go
 
-df = pd.read_csv('../vehicles_us.csv')
+df = pd.read_csv('vehicles_us.csv')
 
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
